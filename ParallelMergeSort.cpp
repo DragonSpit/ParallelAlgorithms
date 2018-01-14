6abb3507d82aeef685f54fb3641579f6e784f8f9
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2018-01-14 13:54:34 -0500

    Added Listing 3 and 4 for the blog and article: Parallel Merge Sort

--- ParallelMergeSort.cpp
+++ ParallelMergeSort.cpp
@@ -10,6 +10,7 @@
 // two behaviors more obvious and explicit and not even have srcToDst argument.
 // Indexes l and r must be int's to provide the ability to specify zero elements with l = 0 and r = -1.  Otherwise, specifying zero would be a little strange
 // and you'd have to do it as l = 1 and r = 0. !!! This may be the reason that STL does *src_start and *src_end, and then the wrapper function may not be needed!!!
+
 // Listing 1
 template< class _Type >
 inline void parallel_merge_sort_simplest_r( _Type* src, int l, int r, _Type* dst, bool srcToDst = true )	// srcToDst specifies direction for this level of recursion
@@ -27,9 +28,70 @@ inline void parallel_merge_sort_simplest_r( _Type* src, int l, int r, _Type* dst
 	if ( srcToDst ) merge_parallel_L5( src, l, m, m + 1, r, dst, l );
 	else			merge_parallel_L5( dst, l, m, m + 1, r, src, l );
 }
+
 template< class _Type >
 inline void parallel_merge_sort_simplest( _Type* src, int l, int r, _Type* dst, bool srcToDst = true )	// srcToDst specifies direction for this level of recursion
 {
 	if ( r < l ) return;
 	parallel_merge_sort_simplest_r( src, l, r, dst, srcToDst );
+}
+
+// Listing 2
+template< class _Type >
+inline void parallel_merge_sort( _Type* src, int l, int r, _Type* dst )
+{
+    parallel_merge_sort_hybrid( src, l, r, dst, true );  // srcToDst = true
+}
+ 
+template< class _Type >
+inline void parallel_merge_sort_pseudo_inplace( _Type* srcDst, int l, int r, _Type* aux )
+{
+    parallel_merge_sort_hybrid( srcDst, l, r, aux, false );  // srcToDst = false
+}
+
+// Listing 3
+template< class _Type >
+inline void parallel_merge_sort_hybrid_rh( _Type* src, int l, int r, _Type* dst, bool srcToDst = true )
+{
+    if ( r == l ) {    // termination/base case of sorting a single element
+        if ( srcToDst )  dst[ l ] = src[ l ];    // copy the single element from src to dst
+        return;
+    }
+    if (( r - l ) <= 48 ) {
+        insertionSortSimilarToSTLnoSelfAssignment( src + l, r - l + 1 );        // in both cases sort the src
+        //stable_sort( src + l, src + r + 1 );  // STL stable_sort can be used instead, but is slightly slower than Insertion Sort
+        if ( srcToDst ) for( int i = l; i <= r; i++ )    dst[ i ] = src[ i ];    // copy from src to dst, when the result needs to be in dst
+        return; 
+    }
+    int m = ( r + l ) / 2;
+    //tbb::parallel_invoke(             // Intel's     Threading Building Blocks (TBB)
+    Concurrency::parallel_invoke(       // Microsoft's Parallel Pattern Library  (PPL)
+        [&] { parallel_merge_sort_hybrid_rh( src, l,     m, dst, !srcToDst ); },        // reverse direction of srcToDst for the next level of recursion
+        [&] { parallel_merge_sort_hybrid_rh( src, m + 1, r, dst, !srcToDst ); }     // reverse direction of srcToDst for the next level of recursion
+    );
+    if ( srcToDst ) merge_parallel_L5( src, l, m, m + 1, r, dst, l );
+    else            merge_parallel_L5( dst, l, m, m + 1, r, src, l );
+}
+
+// Listing 4
+template< class _Type >
+inline void parallel_merge_sort_hybrid_rh_1( _Type* src, int l, int r, _Type* dst, bool srcToDst = true )
+{
+    if ( r == l ) {    // termination/base case of sorting a single element
+        if ( srcToDst )  dst[ l ] = src[ l ];    // copy the single element from src to dst
+        return;
+    }
+    if (( r - l ) <= 48 && !srcToDst ) {     // 32 or 64 or larger seem to perform well
+        insertionSortSimilarToSTLnoSelfAssignment( src + l, r - l + 1 );    // want to do dstToSrc, can just do it in-place, just sort the src, no need to copy
+        //stable_sort( src + l, src + r + 1 );  // STL stable_sort can be used instead, but is slightly slower than Insertion Sort
+        return; 
+    }
+    int m = (( r + l ) / 2 );
+    //tbb::parallel_invoke(             // Intel's     Threading Building Blocks (TBB)
+    Concurrency::parallel_invoke(       // Microsoft's Parallel Pattern Library  (PPL)
+        [&] { parallel_merge_sort_hybrid_rh_1( src, l,     m, dst, !srcToDst ); },      // reverse direction of srcToDst for the next level of recursion
+        [&] { parallel_merge_sort_hybrid_rh_1( src, m + 1, r, dst, !srcToDst ); }       // reverse direction of srcToDst for the next level of recursion
+    );
+    if ( srcToDst ) merge_parallel_L5( src, l, m, m + 1, r, dst, l );
+    else            merge_parallel_L5( dst, l, m, m + 1, r, src, l );
 }
