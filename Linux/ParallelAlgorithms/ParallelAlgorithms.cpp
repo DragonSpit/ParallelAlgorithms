9e3c0f37704fcdbd7d84adae77b81e00cae878cd
Victor Duvanenko <vduvanenko1@gmail.com>
2020-09-05 21:36:19 -0400

    Initial CMake-based project for Linux

---
+++ Linux/ParallelAlgorithms/ParallelAlgorithms.cpp
@@ -0,0 +1,12 @@
+﻿// ParallelAlgorithms.cpp : Defines the entry point for the application.
+//
+
+#include "ParallelAlgorithms.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
