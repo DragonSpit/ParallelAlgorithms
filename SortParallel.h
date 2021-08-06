8065d755f020ae97aa6a9a681c74a6e926592483
Victor Duvanenko <vduvanenko1@gmail.com>
2021-08-05 22:54:47 -0400

    Improved interface on parallel merge sort with dst buffer, by requiring size of the dst buffer to be specified, and only providing the fastest algorithm version

--- SortParallel.h
+++ SortParallel.h
@@ -44,31 +44,35 @@ namespace ParallelAlgorithms
         }
     }
 
-    // Array bounds includes l/left, but does not include r/right
-    // dst buffer must be large enough to provide elements dst[l to r-1]
+    // dst buffer must be the same or larger in size than the src
     // Two use cases:
     //   -     in-place interface, where the dst buffer is a temporary work buffer
     //   - not-in-place interface, where the dst buffer is the destination memory buffer
     template< class _Type >
-    inline void sort_par(_Type* src, size_t src_size, _Type* dst, bool srcToDst = true)
+    inline void sort_par(_Type* src, size_t src_size, _Type* dst, size_t dst_size, bool srcToDst = false)
     {
         if (!dst)
-            sort(std::execution::par_unseq, src + 0, src + src_size);
-        else
-            ParallelAlgorithms::parallel_merge_sort_hybrid_rh_1(src, 0, src_size - 1, dst, srcToDst);    // size - 1 because this algorithm wants inclusive bounds
+            throw std::invalid_argument("dst is null, which is not supported");
+        if (dst_size < src_size)
+            throw std::invalid_argument("dst_size must be larger or equal to src_size");
+
+        ParallelAlgorithms::parallel_merge_sort_hybrid_rh_1(src, 0, src_size - 1, dst, srcToDst);    // size - 1 because this algorithm wants inclusive bounds
     }
 
     // Array bounds includes l/left, but does not include r/right
-    // dst buffer must be large enough to provide elements dst[l to r-1]
+    // dst buffer must be large enough to provide elements dst[0 to r-1], as the result is placed in dst[l to r-1]
     // Two use cases:
     //   -     in-place interface, where the dst buffer is a temporary work buffer
     //   - not-in-place interface, where the dst buffer is the destination memory buffer
     template< class _Type >
-    inline void sort_par(_Type* src, size_t l, size_t r, _Type* dst, bool srcToDst = true)
+    inline void sort_par(_Type* src, size_t l, size_t r, _Type* dst, size_t dst_size, bool srcToDst = false)
     {
         if (!dst)
-            sort(std::execution::par_unseq, src + l, src + r);
-        else
-            ParallelAlgorithms::parallel_merge_sort_hybrid_rh_1(src, l, r - 1, dst, srcToDst);    // r - 1 because this algorithm wants inclusive bounds
+            throw std::invalid_argument("dst is null, which is not supported");
+        size_t src_size = r;
+        if (dst_size < src_size)
+            throw std::invalid_argument("dst_size must be larger or equal to r, to be able to return dst[l to r-1]");
+
+        ParallelAlgorithms::parallel_merge_sort_hybrid_rh_1(src, l, r - 1, dst, srcToDst);    // r - 1 because this algorithm wants inclusive bounds
     }
 }
