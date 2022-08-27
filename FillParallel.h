fd0a0bc082d1dadaa5594ddd5334dd9aff07c6ef
Victor Duvanenko <vduvanenko1@gmail.com>
2022-08-27 16:20:30 -0400

    Implemented a generic parallel Fill algorithm, which works for many data types and is parallel scaling well, showing the benefit of parallelism even for an array fill on machines with lots of cores and many memory channels

--- FillParallel.h
+++ FillParallel.h
@@ -9,6 +9,7 @@
 #include <thread>
 #include <execution>
 #include <ppl.h>
+#include <algorithm>
 #else
 #include <iostream>
 #include <algorithm>
@@ -22,15 +23,17 @@
 
 namespace ParallelAlgorithms
 {
-    inline void parallel_fill_r(unsigned char* src, unsigned char value, std::size_t l, std::size_t r, std::size_t parallel_threshold = 16 * 1024)
+    // Inclusive-left and exclusive-right boundaries
+    template< class _Type >
+    inline void parallel_fill(_Type* src, _Type value, size_t l, size_t r, size_t parallel_threshold = 16 * 1024)
     {
-        if (r < l)
+        if (r <= l)
             return;
-        if ((r - l + 1) < parallel_threshold)
+        if ((r - l) < parallel_threshold)
         {
-            //memset(src + l, (int)value, r - l + 1);
-            for (size_t i = l; i <= r; i++)
-                src[i] = value;
+            std::fill(src + l, src + r, value);     // many times faster than for loop
+            //for (size_t i = l; i < r; i++)
+            //    src[i] = value;
             return;
         }
         size_t m = r / 2 + l / 2 + (r % 2 + l % 2) / 2;     // average without overflow
@@ -39,46 +42,32 @@ namespace ParallelAlgorithms
 #else
         tbb::parallel_invoke(
 #endif
-            // TODO: Figure out why when enabling the next two lines crashes!
-            //[&] { parallel_fill_r(src, value, l,     m, parallel_threshold); },
-            //[&] { parallel_fill_r(src, value, m + 1, r, parallel_threshold); }
-            [&] { memset(src + l,     value, m - l + 1      ); },
-            [&] { memset(src + m + 1, value, r - (m + 1) + 1); }
-            //memset(src + l, (int)value, r - l + 1);
+            [&] { parallel_fill(src, value, l, m, parallel_threshold); },
+            [&] { parallel_fill(src, value, m, r, parallel_threshold); }
         );
     }
-
-    inline void parallel_fill_r2(unsigned char* src, unsigned char value, std::size_t start_index, std::size_t length, std::size_t parallel_threshold = 16 * 1024)
+    // Inclusive-left and exclusive-right boundaries
+    inline void parallel_fill(unsigned char* src, unsigned char value, size_t l, size_t r, size_t parallel_threshold = 16 * 1024)
     {
-        //printf("pfr2: start_index = %zu   length = %zu\n", start_index, length);
-        if (length < parallel_threshold)
+        if (r <= l)
+            return;
+        if ((r - l) < parallel_threshold)
         {
-            memset(src + start_index, (int)value, length);
-            //size_t end_index = start_index + length;
-            //for (size_t i = start_index; i < end_index; i++)
+            //memset(src + l, (int)value, r - l);   // many times faster than the for loop below
+            std::fill(src + l, src + r, value);     // same performance as memset
+            //for (size_t i = l; i < r; i++)
             //    src[i] = value;
             return;
         }
-        size_t m = length / 2;
-        size_t length_of_2nd_half = length - m;
+        size_t m = r / 2 + l / 2 + (r % 2 + l % 2) / 2;     // average without overflow
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
         Concurrency::parallel_invoke(
 #else
         tbb::parallel_invoke(
 #endif
-            [=] { parallel_fill_r2(src, value, start_index,     m,                  parallel_threshold); },
-            [=] { parallel_fill_r2(src, value, start_index + m, length_of_2nd_half, parallel_threshold); }
-            //[&] { memset(src + l, value, m - l + 1); },
-            //[&] { memset(src + m + 1, value, r - (m + 1) + 1); }
-            //memset(src + l, (int)value, r - l + 1);
+            [&] { parallel_fill(src, value, l, m, parallel_threshold); },
+            [&] { parallel_fill(src, value, m, r, parallel_threshold); }
         );
-    }
-
-    template< class _Type >
-    inline void parallel_fill(_Type* src, _Type value, std::size_t l, std::size_t r, std::size_t parallel_threshold = 16 * 1024)
-    {
-        if (r < l) return;
-        parallel_fill_r(src, value, l, r, parallel_threshold);
     }
 }
 #endif
