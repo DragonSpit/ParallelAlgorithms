937d8786863c22a6f41312f0b87294a0cb7fb2a8
Victor Duvanenko <vduvanenko1@gmail.com>
2022-10-09 09:51:04 -0400

    Added non-recursive Addition parallel implementations

--- SumParallel.h
+++ SumParallel.h
@@ -1,3 +1,4 @@
+// TODO: Implement a more efficient suggestion of using task_group to split the array into chunks with each returning its sum into one index of an array of sums
 #pragma once
 
 // Parallel Sum implementations
@@ -7,11 +8,11 @@
 
 //#include <cstddef>
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-#include <thread>
-#include <execution>
-#include <ppl.h>
-#else
+//#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+//#include <thread>
+//#include <execution>
+//#include <ppl.h>
+//#else
 #include <iostream>
 #include <algorithm>
 #include <chrono>
@@ -21,7 +22,10 @@
 #include <thread>
 #include <execution>
 #include <tbb/parallel_invoke.h>
-#endif
+//#endif
+
+#define __TBB_PREVIEW_TASK_ARENA_CONSTRAINTS_EXTENSION_PRESENT 1
+#include <oneapi/tbb/task_arena.h>
 
 #include "RadixSortMsdParallel.h"
 #include "FillParallel.h"
@@ -44,15 +48,15 @@ void print_results(const char* const tag, const unsigned long long sum, size_t s
 
 namespace ParallelAlgorithms
 {
-// left (l) boundary is inclusive and right (r) boundary is exclusive
-inline unsigned long long Sum(unsigned long long in_array[], size_t l, size_t r)
-{
-	unsigned long long sum = 0;
-	for (size_t current = l; current < r; current++)
-		sum += in_array[current];
-	//unsigned long long sum_left = std::accumulate(in_array + l, in_array + r, 0);	// may be implemented using SIMD/SSE
-	return sum;
-}
+	// left (l) boundary is inclusive and right (r) boundary is exclusive
+	inline unsigned long long Sum(unsigned long long in_array[], size_t l, size_t r)
+	{
+		unsigned long long sum = 0;
+		for (size_t current = l; current < r; current++)
+			sum += in_array[current];
+		//unsigned long long sum_left = std::accumulate(in_array + l, in_array + r, 0);	// may be implemented using SIMD/SSE
+		return sum;
+	}
 
 	// left (l) boundary is inclusive and right (r) boundary is exclusive
 	inline unsigned long long SumParallel(unsigned long long in_array[], size_t l, size_t r, size_t parallelThreshold = 16 * 1024)
@@ -111,6 +115,80 @@ inline unsigned long long Sum(unsigned long long in_array[], size_t l, size_t r)
 		sum_left += sum_right;
 
 		return sum_left;
+	}
+	// Non-recursive Sum
+	// left (l) boundary is inclusive and right (r) boundary is exclusive
+	inline unsigned long long SumNonRecursive(unsigned long long in_array[], size_t l, size_t r, size_t parallelThreshold = 16 * 1024)
+	{
+		size_t num_tasks = (r - l + (parallelThreshold - 1)) / parallelThreshold;
+		unsigned long long* sum_array = new unsigned long long[num_tasks] {};
+
+		size_t i = 0;
+		for (; i < (num_tasks - 1); i++)
+			sum_array[i] = Sum(in_array, l + parallelThreshold * i, l + parallelThreshold * (i + 1));	// process full parallelThreshold chunks
+
+		sum_array[num_tasks - 1] = Sum(in_array, l + parallelThreshold * i, r);	// process the last partial parallelThreshold chunk
+
+		unsigned long long sum = 0;
+		for (size_t i = 0; i < num_tasks; i++)
+			sum += sum_array[i];
+
+		delete[] sum_array;
+		return sum;
+	}
+
+	// Non-recursive Parallel Sum
+	// left (l) boundary is inclusive and right (r) boundary is exclusive
+	inline unsigned long long SumParallelNonRecursive(unsigned long long in_array[], size_t l, size_t r, size_t parallelThreshold = 16 * 1024)
+	{
+		size_t num_tasks = (r - l + (parallelThreshold - 1)) / parallelThreshold;
+		unsigned long long* sum_array = new unsigned long long[num_tasks] {};
+		tbb::task_group g;
+
+		size_t i = 0;
+		for (; i < (num_tasks - 1); i++)
+			g.run([=] {sum_array[i] = Sum(in_array, l + parallelThreshold * i, l + parallelThreshold * (i + 1)); });	// process full parallelThreshold chunks
+
+		g.run([=] {sum_array[num_tasks - 1] = Sum(in_array, l + parallelThreshold * i, r); });	// process the last partial parallelThreshold chunk
+
+		g.wait();	// wait for all tasks to complete
+
+		unsigned long long sum = 0;
+		for (size_t i = 0; i < num_tasks; i++)
+			sum += sum_array[i];
+
+		delete[] sum_array;
+		return sum;
+	}
+
+	// Non-recursive Parallel Sum without Hyperthreading
+	// left (l) boundary is inclusive and right (r) boundary is exclusive
+	inline unsigned long long SumParallelNonRecursiveNoHyperthreading(unsigned long long in_array[], size_t l, size_t r, size_t parallelThreshold = 16 * 1024)
+	{
+		size_t num_tasks = (r - l + (parallelThreshold - 1)) / parallelThreshold;
+		unsigned long long* sum_array = new unsigned long long[num_tasks] {};
+
+		int no_ht_concurrency = tbb::info::default_concurrency(
+			tbb::task_arena::constraints{}.set_max_threads_per_core(1)
+		);
+		tbb::task_arena arena(no_ht_concurrency);
+		arena.execute([=] {
+			tbb::task_group g;
+			size_t i = 0;
+			for (; i < (num_tasks - 1); i++)
+				g.run([=] {sum_array[i] = Sum(in_array, l + parallelThreshold * i, l + parallelThreshold * (i + 1)); });	// process full parallelThreshold chunks
+
+			g.run([=] {sum_array[num_tasks - 1] = Sum(in_array, l + parallelThreshold * i, r); });	// process the last partial parallelThreshold chunk
+
+			g.wait();	// wait for all tasks to complete
+		});
+
+		unsigned long long sum = 0;
+		for (size_t i = 0; i < num_tasks; i++)
+			sum += sum_array[i];
+
+		delete[] sum_array;
+		return sum;
 	}
 }
 
